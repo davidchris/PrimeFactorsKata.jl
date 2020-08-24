f722a9939bd2529070ef5151166c076b197ef1b4
David Wilde <davidchris.gerhardt@gmail.com>
2020-08-24 09:12:30 +0200

    working version

--- src/PrimeFactorsKata.jl
+++ src/PrimeFactorsKata.jl
@@ -1,5 +1,18 @@
 module PrimeFactorsKata
 
-# Write your package code here.
-
+function factorsOf(x)
+    factors = Int[]
+    divisor = 2
+    while x > 1
+        while x % divisor == 0
+            append!(factors, divisor)
+            x = convert(Int, x / divisor)
+        end
+        divisor += 1  
+    end 
+    return factors
 end
+
+export factorsOf
+
+end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,14 @@ using PrimeFactorsKata
 using Test
 
 @testset "PrimeFactorsKata.jl" begin
-    # Write your tests here.
+    @test factorsOf(1) == []
+    @test factorsOf(2) == [2]
+    @test factorsOf(3) == [3]
+    @test factorsOf(4) == [2, 2]
+    @test factorsOf(5) == [5]
+    @test factorsOf(6) == [2, 3]
+    @test factorsOf(7) == [7]
+    @test factorsOf(8) == [2, 2, 2]
+    @test factorsOf(9) == [3, 3]
+    @test factorsOf(2 * 2 * 3 * 5 * 7 * 7 * 11 * 13 * 13) == [2,2,3,5,7,7,11,13,13]
 end
